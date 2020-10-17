da361d4559f77b66ef0b8da5cae85e484f23f335
CHOI HEE JAE <gmlwo530@gmail.com>
2020-10-17 17:06:33 +0900

    Add github url to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,15 @@ import "../sass/footer.scss";
 export const Footer: React.FC = () => {
   return (
     <footer>
+      <div className="links">
+        <a
+          href="https://github.com/gmlwo530/dollar-to-food-emoji"
+          target="blank"
+        >
+          Github
+        </a>
+      </div>
+
       <div className="copyright">
         2020 &copy;{" "}
         <a href="https://github.com/gmlwo530" target="blank">
